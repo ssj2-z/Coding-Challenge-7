7ed13ff0b1d1664ad350d15a2cb4f3b07d228822
ssj2-z <ssj2@usf.edu>
2025-02-11 17:54:35 -0500

    Task 2 - Employee Hourly Wage Calculation.

--- cc_7.js
+++ cc_7.js
@@ -8,3 +8,14 @@ calculateInvoice(100, .08, 5);
 // Expected output: "Total Invoice: $103"
 calculateInvoice(500, .1, 20); 
 // Expected output: "Total Invoice: $530"
+
+// Task 2 Calculate Employee hours
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    let hourlyWage = salary / (hoursPerWeek * 52); 
+    console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`)
+}
+
+// Test Cases 
+calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25"
+calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
+
